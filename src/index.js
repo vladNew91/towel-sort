592cf04dbf4282ac17b723d89cba311954153f39
vladNew91 <vladnew91@gmail.com>
2020-10-15 16:23:20 +0300

    init: towel-sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (matrix) {
+        if (matrix[1]) {
+            matrix[1].reverse();
+        }
+        if (matrix[3]) {
+            matrix[3].reverse();
+        }
+        let a = matrix.flat();
+        return a;
+    } else return [];
+};
